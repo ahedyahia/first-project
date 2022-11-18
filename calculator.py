f76079e87f26a7944cff18c8e078106898ccdd04
ahedyahia <118531699+ahedyahia@users.noreply.github.com>
2022-11-18 03:31:32 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+number1= int (input ("enter first number"))
+op= input ("/,*,-,+ __>")
+number2= int (input ("enter second number"))
+if op =="+":
+    print(number1+number2)
+elif op =="/":
+    print(number1/number2)
+elif op =="*":
+    print(number1*number2)
+elif op =="-":
+    print(number1-number2)
