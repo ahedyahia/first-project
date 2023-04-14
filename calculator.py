227e6c4ffe5adebce75c2a69f8220c57b9188f54
ahedyahia <118531699+ahedyahia@users.noreply.github.com>
2023-04-14 09:28:46 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,45 @@
-number1= int (input ("enter first number"))
-op= input ("/,*,-,+ __>")
-number2= int (input ("enter second number"))
-if op =="+":
+import pyttsx3
+yahia=pyttsx3.init()
+
+
+loop=int(1)
+while loop ==1:
+ yahia.say('enter your first number')
+ yahia.runAndWait()
+
+ number1= int (input ("enter first number"))
+ yahia.say('enter the operation')
+ yahia.runAndWait()
+
+ op= input ("/,*,-,+ ==>")
+ yahia.say('enter your second number')
+ yahia.runAndWait()
+ number2= int (input ("enter second number"))
+ 
+
+ if op =="+":
+    yahia.say('the result is')
+    yahia.runAndWait()
+    yahia.say(number1+number2)
+    yahia.runAndWait()
     print(number1+number2)
-elif op =="/":
+    
+ elif op =="/":
+    yahia.say('the result is')
+    yahia.runAndWait()
+    yahia.say(number1/number2)
+    yahia.runAndWait()
     print(number1/number2)
-elif op =="*":
+ elif op =="*":
+    yahia.say('the result is')
+    yahia.runAndWait()
+    yahia.say(number1*number2)
+    yahia.runAndWait()
     print(number1*number2)
-elif op =="-":
+ elif op =="-":
+    yahia.say('the result is')
+    yahia.runAndWait()
+    yahia.say(number1-number2)
+    yahia.runAndWait()
     print(number1-number2)
+
